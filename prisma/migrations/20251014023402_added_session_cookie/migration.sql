235f612f2087ae29bbe7e811220a0ef1bf0e3579
luan-services <luandotservices@gmail.com>
2025-10-13 23:36:13 -0300

    feat: added session model to prisma database

---
+++ prisma/migrations/20251014023402_added_session_cookie/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "refreshHash" TEXT NOT NULL,
+    "userAgent" TEXT,
+    "ip" TEXT,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_id_fkey" FOREIGN KEY ("id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
